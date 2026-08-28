1d789089b94a492bc15b027a000dde251118f627
Samuel Salcedo <smolinasalcedo@gmail.com>
2026-08-28 16:38:00 -0500

    2) Creacion de las 4 clases;  Persona, Animal, Alienigena, Planeta

--- Polimorfismo/src/polimorfismo/Polimorfismo.java
+++ Polimorfismo/src/polimorfismo/Polimorfismo.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package polimorfismo;
 
-/**
- *
- * @author PC
- */
-public class Polimorfismo {
+public class Polimorfismo 
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        
+    
+    
+    
+    
+    
     }
     
 }
