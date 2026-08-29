bff08d54f4c1295bb5981f34fcaf09fae3e2ede6
Samuel Salcedo <smolinasalcedo@gmail.com>
2026-08-28 19:38:22 -0500

    9) Creacion de los metodos constructores para la clase Epoca

--- Polimorfismo/src/polimorfismo/Epoca.java
+++ Polimorfismo/src/polimorfismo/Epoca.java
@@ -8,7 +8,24 @@ public class Epoca
   boolean fantasia = false;
   
   
- 
+  public Epoca(String nombreEpoca){
+  
+  this.nombreEpoca = nombreEpoca;
+  }
+  
+  public Epoca(String nombreEpoca, int año){
+  
+    this.nombreEpoca = nombreEpoca;
+    this.año = año;
+  }
+  
+  public Epoca(String nombreEpoca, int año, boolean fantasia){
+  
+    this.nombreEpoca = nombreEpoca;
+    this.año = año;
+    this.fantasia = fantasia;
+  
+  }
 
 
 
